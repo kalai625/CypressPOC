f86d4498121296a73e38b40797c5f9191507dd6f
KalaiAnnular <kalaiyarasan@annulartechnologies.com>
2023-03-27 15:22:03 +0530

    first commit

---
+++ cypress/e2e/Test1.cy.js
@@ -0,0 +1,12 @@
+describe('template spec', () => {
+ 
+  beforeEach(() => {
+    cy.visit('https://abs-ops-portal-test.azurewebsites.net/')
+    console.log("print 1")
+  })
+
+  it('should display the homepage', () => {
+   console.log("print 2")
+  })
+
+})
